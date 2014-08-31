2af8cf04f189b7f1ff4d1da43804a1aed3b50a86
BubberDunk <samuelgrivas@hotmail.com>
2014-08-31 15:22:15 -0700

    Fixed version

--- albumproject.py
+++ albumproject.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python2
+import os
 import musicbrainzngs as mb
 import beets
 
@@ -6,24 +8,30 @@ artistList = []
 localList = []
 brainzList = {}
 mbList = []
-lib = beets.Library(path='/home/bubber/.config/beets/library.db')
+homeDir = os.environ['HOME']
+lib = beets.Library(path=homeDir + '/.config/beets/library.db')
 
-def getMBID(): 
+
+def getMBID():
     for artists in lib.albums():
         if len(artists['mb_albumartistid']) == 0:
             pass
         else:
             artistList.append(artists['mb_albumartistid'])
-            #localList.append(artists['mb_albumid'])
+            # localList.append(artists['mb_albumid'])
             localList.append(artists['album'])
     for albums in set(artistList):
-        brainzList.update(mb.browse_release_groups(artist=albums)) #online album db
+        # online album db
+        brainzList.update(mb.browse_release_groups(artist=albums))
     for info in brainzList['release-group-list']:
         mbList.append(info['title'])
 
-    #def compare():
+    # def compare():
     for mbid in mbList:
-         if mbid in localList:
-             pass
-         else:
+        if mbid in localList:
+            pass
+        else:
             print mbid
+
+if __name__ == '__main__':
+    getMBID()
